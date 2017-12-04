51bbccc04b370bea83779dc0691630d64751fd99
Kevin Griesser <ktg10@case.edu>
2017-12-04 14:31:10 -0500

    fixed query syntax

--- dbRequests.py
+++ dbRequests.py
@@ -7,7 +7,7 @@ def getAppointmentHistory(animal_id):
     conn = sqlite3.connect(DB_NAME)
     c = conn.cursor()
 
-    c.execute("select * from Appointments as a where a.a_id=?", animal_id)
+    c.execute("select * from Appointments as a where a.a_id=?", (animal_id,))
     ret = c.fetchall()
 
     conn.close()
@@ -18,8 +18,22 @@ def getPaymentRecords(user_id):
     conn = sqlite3.connect(DB_NAME)
     c = conn.cursor()
 
-    c.execute("select * from Payments as p where p.o_id=?", user_id)
+    c.execute("select * from Payments as p where p.o_id=?", (user_id,))
     ret = c.fetchall()
 
     conn.close()
     return ret
+
+
+def getPetAppointments(animal_id, date):
+    conn = sqlite3.connect(DB_NAME)
+    c = conn.cursor()
+
+    c.execute("select * from Appointments as a where a.a_id=? and a.date > ?", (animal_id, date))
+    ret = c.fetchall()
+
+    conn.close()
+    return ret
+
+
+
